a9871e11aa626d8507372dea3fda9bf1f1a428ec
surekhasrinivasan <surekhasrao@gmail.com>
2018-05-15 13:48:35 -0400

    Edex Module-1 class_demo

---
+++ Module_1/mod1_class_demo/class_demo/DrinksMachine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace class_demo
+{
+    class DrinksMachine
+    {
+        //The following statements declare public member variables 
+        public string _location;
+        public string _make;
+        public string _model;
+        
+        public void MakeCappuccino()
+        {
+            Console.WriteLine("Cappuccino is made.");
+        }
+        public void MakeEspresso()
+        {
+            // Method logic goes here.
+        }
+    }
+}
---
+++ Module_1/mod1_class_demo/class_demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace class_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine();
+            myMachine._location = "Kitchen";
+            myMachine._model = "DM1000";
+            Console.WriteLine(myMachine._location);
+            myMachine.MakeCappuccino();
+        }
+    }
+}
