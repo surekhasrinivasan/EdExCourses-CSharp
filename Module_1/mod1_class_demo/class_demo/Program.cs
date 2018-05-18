7851fdbc1109da9e1382a22286ba4baf95782097
surekhasrinivasan <surekhasrao@gmail.com>
2018-05-18 15:40:16 -0400

    static class demo

--- Module_1/mod1_class_demo/class_demo/Program.cs
+++ Module_1/mod1_class_demo/class_demo/Program.cs
@@ -2,18 +2,12 @@
 
 namespace class_demo
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            DrinksMachine myMachine = new DrinksMachine("Kitchen","Brand", "DM1000");
-            //myMachine.Location = "Kitchen";
-            //myMachine.Model = "DM1000";
-            Console.WriteLine(myMachine.Location);
-            Console.WriteLine(myMachine.Make);
-            Console.WriteLine(myMachine.Model);
-
-            myMachine.MakeCappuccino();
+            Console.WriteLine(Math.Pow(2, 8));
+            Console.WriteLine(Math.Pow(2, 10));
         }
     }
 }
