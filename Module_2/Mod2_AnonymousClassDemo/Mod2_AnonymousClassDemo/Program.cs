f792a5fbc23a56ea3b3bad705d231c4168ee4d04
surekhasrinivasan <surekhasrao@gmail.com>
2018-05-23 11:37:27 -0400

    Module_2 Anonymous Class Demo

---
+++ Module_2/Mod2_AnonymousClassDemo/Mod2_AnonymousClassDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mod2_AnonymousClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Anon Class
+
+            var anon = new { Name = "Gerry", Age = 29 };
+
+            var anon2 = new { Name = "Tom", Age = 65 };
+
+            Console.WriteLine(anon.Name + " " + anon.Age);
+            Console.WriteLine(anon2.Name + " " + anon2.Age);
+
+            #endregion
+        }
+    }
+}
