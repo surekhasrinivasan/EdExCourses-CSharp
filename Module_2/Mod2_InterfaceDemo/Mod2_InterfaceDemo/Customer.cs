d399718906b918dd6d395dbe4f3b1a54d84fdd8e
surekhasrinivasan <surekhasrao@gmail.com>
2018-05-30 11:12:18 -0400

    Module_2 Interface Demo

---
+++ Module_2/Mod2_InterfaceDemo/Mod2_InterfaceDemo/Customer.cs
@@ -0,0 +1,33 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_InterfaceDemo
+{
+    class Customer : ILoyaltyCardHolder
+    {
+        private int totalPoints;
+
+        public int TotalPoints
+        {
+            get { return totalPoints; }
+        }
+
+        public int AddPoints(decimal transactionValue)
+        {
+            int points = Decimal.ToInt32(transactionValue);
+            totalPoints += points;
+            return totalPoints;
+        }
+
+        public void ResetPoints()
+        {
+            totalPoints = 0;
+        }
+
+        //Other members of the Customer class.
+    }
+}
+
---
+++ Module_2/Mod2_InterfaceDemo/Mod2_InterfaceDemo/ILoyaltyCardHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2_InterfaceDemo
+{
+    interface ILoyaltyCardHolder
+    {
+        int TotalPoints { get; }
+
+        int AddPoints(decimal transactionValue);
+
+        void ResetPoints();
+    }
+}
